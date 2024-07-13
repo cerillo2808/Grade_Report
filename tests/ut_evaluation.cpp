381b2d7c71d11c3d3232ca5838ec6c2f59e85536
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2024-07-13 15:07:51 -0600

    pruebas nuevas

--- tests/ut_evaluation.cpp
+++ tests/ut_evaluation.cpp
@@ -1,7 +1,1 @@
 #include <gtest/gtest.h>
-
-TEST(Prueba, devolverTrue) {
-  bool verdadero = true;
-
-  EXPECT_TRUE(verdadero);
-}
--- tests/ut_subject.cpp
+++ tests/ut_subject.cpp
@@ -1,1 +1,46 @@
 #include <gtest/gtest.h>
+#include <Evaluation.hpp>
+#include <Type.hpp>
+
+#include <Evaluation.hpp>
+#include <Type.hpp>
+
+TEST(Evaluation, getTypeOther) {
+  Evaluation evaluation = Evaluation(other, 10);
+  Type type = evaluation.getType();
+
+  EXPECT_EQ(type, other);
+}
+
+TEST(Evaluation, getTypeExam) {
+  Evaluation evaluation = Evaluation(exam, 10);
+  Type type = evaluation.getType();
+
+  EXPECT_EQ(type, exam);
+}
+
+TEST(Evaluation, getTypeProject) {
+  Evaluation evaluation = Evaluation(project, 10);
+  Type type = evaluation.getType();
+
+  EXPECT_EQ(type, project);
+}
+
+TEST(Evaluation, getTypeHomework) {
+  Evaluation evaluation = Evaluation(homework, 10);
+  Type type = evaluation.getType();
+
+  EXPECT_EQ(type, homework);
+}
+
+TEST(Evaluation, getTypeQuiz) {
+  Evaluation evaluation = Evaluation(quiz, 10);
+  Type type = evaluation.getType();
+
+  EXPECT_EQ(type, quiz);
+}
+
+TEST(Evaluation, falso){
+    bool falso = false;
+    EXPECT_TRUE(falso);
+}
