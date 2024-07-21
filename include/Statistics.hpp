b08935d8bf07f6bada91f0760cd6b40218fb68e4
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2024-07-20 21:30:10 -0600

    implementacion inicial de statistics

--- include/Statistics.hpp
+++ include/Statistics.hpp
@@ -2,12 +2,22 @@
 #define STATISTICS
 #include <Type.hpp>
 #include <iostream>
+#include <vector>
+#include <Subject.hpp>
 #include <string>
 
 using namespace std;
 
 class Statistics {
  public:
+ //if there is an evaluation or subject, it returns true. If it has no evaluations or subjects, returns false.
+ bool anyEvaluation();
+ bool anySubject();
+ bool evaluationExist(Type evaluation, int order);
+ bool subjectExist(string name);
+
+ private:
+ vector<Subject> subjects;
 };
 
 #endif
--- src/Statistics.cpp
+++ src/Statistics.cpp
@@ -1,2 +1,29 @@
-// TO-DO: verificar si hay evaluación o subject
-// TO-DO: linkear a cmake
+#include <Evaluation.hpp>
+#include <Subject.hpp>
+#include <Type.hpp>
+#include <Statistics.hpp>
+#include <iostream>
+
+using namespace std;
+
+bool Statistics:: anyEvaluation(){
+
+    if (subjects.empty()){
+        return false;
+    }
+
+    for (size_t i=0; i<subjects.size(); i++){
+       // if (subjects[i].) TODO: Hacer getter de las evaluaciones de un subject
+    }
+
+
+    
+    return true;
+}
+
+bool Statistics:: anySubject(){
+    if (subjects.empty()){
+        return false;
+    }
+    return true;
+}
