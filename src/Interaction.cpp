ad79cd3dd6e4fba3f498b5526fb207b6d5ff96bb
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2024-07-20 21:14:39 -0600

    clang format

--- src/Interaction.cpp
+++ src/Interaction.cpp
@@ -5,8 +5,6 @@ using namespace std;
 
 void Interaction::HelloWorld() { cout << "Hello Cami" << endl; }
 
-// TO-DO: preguntarle al usuario si quiere crear un archivo de cero o actualizar reporte
-// TO-DO: pedirPathparaFile
-// TO-DO: agregar subject
-// TO-DO: agregar evaluation
-// TO-DO: si es un proyecto, preguntar si es en grupo.
+// TO-DO: preguntarle al usuario si quiere crear un archivo de cero o actualizar
+// reporte TO-DO: pedirPathparaFile TO-DO: agregar subject TO-DO: agregar
+// evaluation TO-DO: si es un proyecto, preguntar si es en grupo.
