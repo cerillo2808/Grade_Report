8ff70090961d7d25731143eb30ad776d0701a2d6
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2024-07-20 21:30:26 -0600

    clang format

--- include/Statistics.hpp
+++ include/Statistics.hpp
@@ -1,23 +1,24 @@
 #ifndef STATISTICS
 #define STATISTICS
+#include <Subject.hpp>
 #include <Type.hpp>
 #include <iostream>
-#include <vector>
-#include <Subject.hpp>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Statistics {
  public:
- //if there is an evaluation or subject, it returns true. If it has no evaluations or subjects, returns false.
- bool anyEvaluation();
- bool anySubject();
- bool evaluationExist(Type evaluation, int order);
- bool subjectExist(string name);
+  // if there is an evaluation or subject, it returns true. If it has no
+  // evaluations or subjects, returns false.
+  bool anyEvaluation();
+  bool anySubject();
+  bool evaluationExist(Type evaluation, int order);
+  bool subjectExist(string name);
 
  private:
- vector<Subject> subjects;
+  vector<Subject> subjects;
 };
 
 #endif
