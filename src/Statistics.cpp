975328375fc16f3e5c8b976622c7d2d452fd38a1
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2024-07-20 21:30:45 -0600

    clang format

--- src/Statistics.cpp
+++ src/Statistics.cpp
@@ -1,29 +1,26 @@
 #include <Evaluation.hpp>
+#include <Statistics.hpp>
 #include <Subject.hpp>
 #include <Type.hpp>
-#include <Statistics.hpp>
 #include <iostream>
 
 using namespace std;
 
-bool Statistics:: anyEvaluation(){
+bool Statistics::anyEvaluation() {
+  if (subjects.empty()) {
+    return false;
+  }
 
-    if (subjects.empty()){
-        return false;
-    }
+  for (size_t i = 0; i < subjects.size(); i++) {
+    // if (subjects[i].) TODO: Hacer getter de las evaluaciones de un subject
+  }
 
-    for (size_t i=0; i<subjects.size(); i++){
-       // if (subjects[i].) TODO: Hacer getter de las evaluaciones de un subject
-    }
-
-
-    
-    return true;
+  return true;
 }
 
-bool Statistics:: anySubject(){
-    if (subjects.empty()){
-        return false;
-    }
-    return true;
+bool Statistics::anySubject() {
+  if (subjects.empty()) {
+    return false;
+  }
+  return true;
 }
